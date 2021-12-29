150e5fbad7b6413913a048382603c95dd8d1a17c
Sunset-kim <wood2002@naver.com>
2021-12-29 23:52:15 +0900

    Framer - motion Study

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import GlobalStyle from "./style/CreateGlobalStyle";
 import Variants from "./components/Variants";
+import Gestures from "./components/Gestures";
+import MotionVal from "./components/MotionVal";
+import SvgAnimation from "./components/SvgAnimation";
+import Presence from "./components/Presence";
+import PresenceSlide from "./components/PresenceSlide";
+import Layout from "./components/Layout";
+import Final from "./components/Final";
 
 const Container = styled.div`
-  background: salmon;
+  background: rgb(250, 128, 114);
   height: 100%;
   display: flex;
   justify-content: center;
@@ -18,7 +25,28 @@ function App() {
       <GlobalStyle />
       <Container>
         {/* 0. framer moiton */}
-        <Variants />
+        {/* <Variants /> */}
+
+        {/* 1. Gestures */}
+        {/* <Gestures /> */}
+
+        {/* 2. Motion Value : 움직이는 양에따른 변화 */}
+        {/* <MotionVal /> */}
+
+        {/* 3. svg : ariBNB logo svg paht length animation */}
+        {/* <SvgAnimation /> */}
+
+        {/* 4. animtaion Presence */}
+        {/* 4.1 basic */}
+        {/* <Presence /> */}
+        {/* 4.2 slide */}
+        {/* <PresenceSlide /> */}
+
+        {/* 5. layout  */}
+        {/* <Layout /> */}
+
+        {/* 6. final modal animation */}
+        <Final />
       </Container>
     </>
   );
